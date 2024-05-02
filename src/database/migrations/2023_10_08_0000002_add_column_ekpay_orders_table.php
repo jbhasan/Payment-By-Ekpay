460ea8164ca81bcec86b6ed9c3e5b76e920bcffb
Hasan <jbhasan@gmail.com>
2024-05-02 18:16:57 +0600

    Commit by Sayeed:

--- src/database/migrations/2023_10_08_0000002_add_column_ekpay_orders_table.php
+++ src/database/migrations/2023_10_08_0000002_add_column_ekpay_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEkpayOrdersTable extends Migration
+class CreateOrderIdColumnOrdersTable extends Migration
 {
     /**
      * Run the migrations.

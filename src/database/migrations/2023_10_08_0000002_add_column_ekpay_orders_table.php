b4cefb22f392dc10565c6c8a19cd3d8537483d8c
Hasan <jbhasan@gmail.com>
2024-05-02 18:22:05 +0600

    Commit by Sayeed:

--- src/database/migrations/2023_10_08_0000002_add_column_ekpay_orders_table.php
+++ src/database/migrations/2023_10_08_0000002_add_column_ekpay_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrderIdColumnOrdersTable extends Migration
+class AddColumnEkpayOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateOrderIdColumnOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::table('ekpay_orders', function (Blueprint $table) {
+		Schema::table('ekpay_orders', function (Blueprint $table) {
             $table->string('order_id')->nullable();
         });
     }
